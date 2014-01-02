c6ce5e5782e2ee978bd7eebd9b4f368fbde9cf5e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-01 16:25:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emilarr::Application.config.secret_key_base = 'd481838c36bf5baf74e30c99fbc9f059e3fd3097088f7c1f83cccff25cf1efcb0a30171fb2392a0abd6400db04cca72de34d8f9e809e82ab3d330f8c5d3236d2'
