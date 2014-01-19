2fe8685a081fe0e6d675aa1a53845f8e655f120d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-18 23:17:21 -0800

    add default bio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def home
-    @bio =  About.last.bio
+    @bio =  About.last.bio ||  "Emily is nice"
     @list = Composer.all.sort_by{|x| x[:name]}
 
   end  
