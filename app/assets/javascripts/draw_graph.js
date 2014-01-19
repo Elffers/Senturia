e5e051107ab177d8e96061a668df5259329e7664
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-18 21:03:46 -0800

    added individual bar canvases

--- app/assets/javascripts/draw_graph.js
+++ app/assets/javascripts/draw_graph.js
@@ -1,64 +1,65 @@
 (function(){ 
   document.addEventListener("DOMContentLoaded", function (e) {
 
-    var canvases = document.querySelectorAll('#bar_graph');
-        // ctx = canvas.getContext('2d');
     var browser_els = document.querySelectorAll('.browser');
 
-    // returns pseudo-array of tds with browser tally
-    var id_rows = document.querySelectorAll('.count');
+    // returns array of browser tally
+    var rows = document.querySelectorAll('.count');
 
     var find_id = function(el){
      return el.id
     };
 
-    // returns array of element ids
-    var ids = Array.prototype.map.call(id_rows, find_id);
+    // returns array of ids
+    var ids = Array.prototype.map.call(rows, find_id);
 
-    var get_count = function(el){
-      return parseInt(el.innerHTML); 
+    var get_count = function(id){
+      return parseInt(document.getElementById(id).innerHTML); 
     };
 
-    // function Browser(el){
-    //     this.id = el.children[1].id;
-    //     this.count = get_count(el.children[1]);
-    // };
-
-    var drawBarrowser = function(el){
-        this.id = el.children[1].id;
-        this.count = get_count(el.children[1]);
-    };
-
-    var browser_array = Array.prototype.slice.call(browser_els, 0);
-    var browsers = browser_array.map(function(el){
-        this.id = el.children[1].id;
-        this.count = get_count(el.children[1]);
-    });
-
-    // returns array of browser tally
     var counts = Array.prototype.map.call(ids, get_count);
 
     var browser_total = counts.reduce(function(a,b){return a + b });
-
-    // returns array of percentages 
-    var stats = counts.map.call(counts, function(count){return ((count/browser_total)*100).toFixed(2)});
-
+    
     // end helpers
 
-    // here, want one argument passed in, which is the element object
-    var drawBar = function(el){
-      var y = ,
+    var Browser = function(el){
+        this.id = el.children[1].id;
+        this.count = parseInt(el.children[1].innerHTML);
+        this.canvas = el.querySelector('#bar_graph');
+        this.ctx = this.canvas.getContext('2d');
+        this.percent = ((this.count/browser_total)*100).toFixed(2);
+    };
+
+    var browser_array = Array.prototype.slice.call(browser_els, 0);
+    // var browsers = browser_array.forEach.call(new Browser);
+
+    var a = new Browser(browser_array[0]);
+    var b = new Browser(browser_array[1]);
+    var c = new Browser(browser_array[2]);
+    var d = new Browser(browser_array[3]);
+    var e = new Browser(browser_array[4]);
+
+    var drawBar = function(el, color){
+      var y = 0,
           x = 0,
-          length = count * 30
-          width = 20;
+          length = el.count * 30
+          width = 20
+          ctx = el.ctx;
         ctx.fillStyle = color;
         ctx.fillRect(x, y, length, width);
         ctx.fillStyle = 'black';
-        ctx.fillText(percent + '%', length + 5, y + width/2 + 5);
+        ctx.fillText(el.percent + '%', length + 5, y + width/2 + 5);
         ctx.fill();
     }; 
 
-   Array.prototype.forEach.call(canvases,drawBar);
+    drawBar(a, '#173B0B');
+    drawBar(b, '#8A0808');
+    drawBar(c, '#0B2161');
+    drawBar(d, '#045FB4');
+    drawBar(e, '#088A08');
+
+   // Array.prototype.forEach.call(canvases,drawBar);
 
   });
 })();
