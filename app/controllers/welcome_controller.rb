b4e0398af9aa07b4e3f6ce0292133d84796cd664
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-18 23:21:18 -0800

    bio debug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def home
-    @bio =  About.last.bio ||  "Emily is nice"
+    if About.last
+      @bio =  About.last.bio
+    else
+      @bio = "Emily is the best"
+    end
     @list = Composer.all.sort_by{|x| x[:name]}
 
   end  
