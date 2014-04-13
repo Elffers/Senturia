fd7e30980c6101dbd3cd8c56620369c61a1e2ad1
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-13 02:29:28 -0700

    refactor @lists to show objects not hardcoded

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,7 @@
 class WelcomeController < ApplicationController
   def home
-    # @links = Link.curent
-    @links =  [ {
-                  url: "http://www.houstongrandopera.org/studio",
-                  name: "Houston Grand Opera Studio"
-                },
-                {
-                  url: 'http://www.aspenmusicfestival.com/students-welcome/programs-of-study/aspen-opera-theater-center/',
-                  name: "Aspen Opera Theater Center"
-                },
-                {
-                  url:'http://www.wolftrap.org/Opera.aspx',
-                  name: "Wolf Trap Opera Company"
-                }
-              ]
+    @links = Link.current
+
     if About.last
       @bio =  About.last.bio
     else
