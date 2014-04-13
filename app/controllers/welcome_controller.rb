68d6e80334c318068a7e6bcc34d1a5187dd4b26b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-12 20:01:48 -0700

    create @lists ivar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   def home
+    @links =  [ {
+                  url: "http://www.houstongrandopera.org/studio",
+                  name: "Houston Grand Opera Studio"
+                },
+                {
+                  url: 'http://www.aspenmusicfestival.com/students-welcome/programs-of-study/aspen-opera-theater-center/',
+                  name: "Aspen Opera Theater Center"
+                },
+                {
+                  url:'http://www.wolftrap.org/Opera.aspx',
+                  name: "Wolf Trap Opera Company"
+                }
+              ]
     if About.last
       @bio =  About.last.bio
     else
