0e503e00a9dc620caa134a4c4cdcb01b8ed3e67e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-10-21 21:35:23 -0700

    resume uploader works

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,15 @@ class AdminController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def home
+    @about = About.last
   end
 
   def edit_bio
-    @about = About.last
+    if About.last
+      @about = About.last
+    else
+      @about = About.new
+    end
   end
 
   def update_bio
@@ -17,12 +22,25 @@ class AdminController < ApplicationController
     end
   end
 
+  def edit_resume
+    @about = About.last
+  end
+
+  def update_resume
+    @about = About.last
+    if @about.update(about_params)
+      redirect_to root_path
+    else
+      render :edit_resume
+    end
+  end
+
   private
   def performance_params
     params.require(:performance).permit(:date, :venue, :program)
   end
 
   def about_params
-    params.require(:about).permit(:bio)
+    params.require(:about).permit(:bio, :resume)
   end
 end
